1faad02e64e5212c0881aa6f9009ea06a3bc7bd2
Tanya Bihag <tbihag@gmail.com>
2019-08-22 22:59:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,33 +8,3 @@ def my_each(array)
     end 
     array
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-describe "my_each" do
-  file = File.read('./my_each.rb')
-
-  it "does not call on .each" do
-    expect(file).to_not include(".each")
-  end
- it "does not call on puts" do
-    expect(file).to_not include("puts")
-  end
-
-  it "calls on while" do
-    expect(file).to include("while")
-  end
-
-  it "iterates over each element" do
-    words = ['hi', 'hello', 'bye', 'goodbye']
