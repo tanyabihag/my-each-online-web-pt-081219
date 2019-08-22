fca2e9371a9f0b9365810dc73a9635c1146e3825
Tanya Bihag <tbihag@gmail.com>
2019-08-22 22:55:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,1 +1,22 @@
 def my_each(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe "my_each" do
+  file = File.read('./my_each.rb')
+
+  it "does not call on .each" do
+    expect(file).to_not include(".each")
+  end
