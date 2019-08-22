82c9911f0d1ecb8234aa85e9767348f8400a62b7
Tanya Bihag <tbihag@gmail.com>
2019-08-22 22:56:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -20,3 +20,13 @@ describe "my_each" do
   it "does not call on .each" do
     expect(file).to_not include(".each")
   end
+ it "does not call on puts" do
+    expect(file).to_not include("puts")
+  end
+
+  it "calls on while" do
+    expect(file).to include("while")
+  end
+
+  it "iterates over each element" do
+    words = ['hi', 'hello', 'bye', 'goodbye']
