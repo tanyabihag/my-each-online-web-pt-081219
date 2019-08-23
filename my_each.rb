0ba54bbf425d9d4ab480d2e398d83fc0ef6fd993
Tanya Bihag <tbihag@gmail.com>
2019-08-23 00:56:44 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+ #if block_given?
+    i = 0 
+    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
+
 end
+
+words = ["hi", "hello", "bye", "goodbye"]
+tas = ['arel', 'jon', 'logan', 'spencer']
+
+my_each(words) do |i|
+  return i
+end
+
+
