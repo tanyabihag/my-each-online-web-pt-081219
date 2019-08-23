d8c4c4a4f295f6a75ec5cd560e865ab22a9613e8
Tanya Bihag <tbihag@gmail.com>
2019-08-23 00:19:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -11,3 +11,10 @@ words = ["hi", "hello", "bye", "goodbye"]
 my_each(words) do |i|
   return i
 end
+
+tas = ['arel', 'jon', 'logan', 'spencer']
+
+my_each(tas) do |ta|
+  return ta
+    end).to contain_exactly('arel', 'jon', 'logan', 'spencer')
+  end
