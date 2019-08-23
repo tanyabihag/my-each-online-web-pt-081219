7ef9cc0221caf1569aa0510bfef774b58f08e76a
Tanya Bihag <tbihag@gmail.com>
2019-08-23 00:40:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,3 @@
-def my_each(array)
-    i = 0 
-    
-    while i < array.length
-      yield array[i]
-      i += 1
-    end
-end
 
-words = ["hi", "hello", "bye", "goodbye"]
-tas = ['arel', 'jon', 'logan', 'spencer']
-my_each(words) do |i|
-  return i
-end
 
 
