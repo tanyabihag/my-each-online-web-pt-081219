64d01807cd2830dbd55ab97f304879e237be0468
Tanya Bihag <tbihag@gmail.com>
2019-08-23 00:43:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,15 @@
 def my_each(array)
+  if block_given?
     i = 0 
     
     while i < array.length
       yield array[i]
       i += 1
     end
+    array
+  else 
+    return tas
+  end
 end
 
 words = ["hi", "hello", "bye", "goodbye"]
